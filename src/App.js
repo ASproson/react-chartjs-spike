f10b78927cc8570ee1e2b25d43a6c010a228a626
Atlas Sproson <atlas.sproson@r2-factory.com>
2022-06-23 15:30:09 +0100

    Adds BarChart component and chartjs-chartjs-2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import BarChart from "./components/BarChart";
 
 function App() {
   return (
     <div className="App">
       <h1>React ChartJS Spike</h1>
+      <BarChart />
     </div>
   );
 }
