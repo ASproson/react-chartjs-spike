88d1b06df0fe39f69003beed43bfecaee845f01e
Atlas Sproson <atlas.sproson@r2-factory.com>
2022-06-24 09:13:15 +0100

    Adds first chart

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import "./App.css";
+import BarChart from "./components/BarChart";
+import { UserData } from "./data/UserData";
 
 function App() {
+  const [userData, setUserData] = useState({
+    labels: UserData.map((d) => d.year),
+    datasets: [
+      {
+        label: "Users gained",
+        data: UserData.map((d) => d.userGain),
+      },
+    ],
+  });
+
   return (
     <div className="App">
       <h1>React ChartJS</h1>
+      <BarChart chartData={userData} />
     </div>
   );
 }
--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Bar } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
 
-const BarChart = () => {
-  return <div></div>;
+const BarChart = ({ chartData }) => {
+  return (
+    <div style={{ width: 600 }}>
+      <Bar
+        data={chartData}
+        // options={}
+      />
+    </div>
+  );
 };
 
 export default BarChart;
